3e0f0acb79a4618ef42c1e092a68169337d75073
sebastianjvf <10047596+sebastianjvf@users.noreply.github.com>
2017-11-14 17:08:38 +0100

    feat: Added a sort by ListPreference.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -109,6 +109,11 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
                 getString(R.string.settings_min_magnitude_key),
                 getString(R.string.settings_min_magnitude_default));
 
+        String orderBy  = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
         // parse breaks apart the URI string that's passed into its parameter
         Uri baseUri = Uri.parse(USGS_REQUEST_URL);
 
@@ -119,7 +124,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
         uriBuilder.appendQueryParameter("format", "geojson");
         uriBuilder.appendQueryParameter("limit", "10");
         uriBuilder.appendQueryParameter("minmag", minMagnitude);
-        uriBuilder.appendQueryParameter("orderby", "time");
+        uriBuilder.appendQueryParameter("orderby", orderBy);
 
         // Create a new loader for the given URL
         return new EarthquakeAsyncLoader(this, uriBuilder.toString());
--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.example.android.quakereport;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -21,12 +22,18 @@ public class SettingsActivity extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
 
+            // Bind the preference summary to their value
             Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
             bindPreferenceSummaryToValue(minMagnitude);
+
+            // Bind the preference summary to their value
+            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderBy);
         }
 
         /**
          * Takes a preference and binds it to the SharedPreferences, when the onCreate method is called.
+         *
          * @param preference
          */
         private void bindPreferenceSummaryToValue(Preference preference) {
@@ -40,7 +47,26 @@ public class SettingsActivity extends AppCompatActivity {
         public boolean onPreferenceChange(Preference preference, Object value) {
             // The code in this method takes care of updating the displayed preference summary after it has been changed
             String stringValue = value.toString();
-            preference.setSummary(stringValue);
+
+            // If the preference is a ListPrefernece
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+
+                // Find the index
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+
+                // Check, if it's over 0
+                if (prefIndex >= 0) {
+                    // Then set get all the labels of the preference lists
+                    CharSequence[] labels = listPreference.getEntries();
+
+                    // And set update the summary with the current value
+                    preference.setSummary(labels[prefIndex]);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+
             return false;
         }
     }
