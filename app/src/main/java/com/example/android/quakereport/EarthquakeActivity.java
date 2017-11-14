1cf5e0d474411a06d5c564cdef9d386f6dd1f0d4
sebastianjvf <sjvf@Sebastians-MacBook-Pro.local>
2017-11-14 09:47:11 +0100

    feat: Uses a loader now to prohibit tasks from being created.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,7 +15,10 @@
  */
 package com.example.android.quakereport;
 
-import android.os.AsyncTask;
+import android.app.LoaderManager;
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
@@ -23,7 +26,7 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EarthquakeActivity extends AppCompatActivity {
+public class EarthquakeActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Earthquake>> {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
     /**
@@ -31,37 +34,71 @@ public class EarthquakeActivity extends AppCompatActivity {
      */
     private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
 
+    /**
+     * Constant value for the earthquake loader ID.
+     */
+    private static final int EARTHQUAKE_LOADER_ID = 1;
+
+    /**
+     * List adapter to be cleared on Loader removal.
+     */
+    EarthquakeListAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        (new EarthquakeAsyncTask()).execute(USGS_REQUEST_URL);
+        getLoaderManager().initLoader(EARTHQUAKE_LOADER_ID, null, this).forceLoad();
     }
 
     /**
-     * Executes a HTTP request in a background threat to load earthquake data.
+     * Returns a new AsyncLoader, passing in the url
+     * @param i
+     * @param bundle
+     * @return
+     */
+    @Override
+    public Loader<List<Earthquake>> onCreateLoader(int i, Bundle bundle) {
+        // Create a new loader for the given URL
+        return new EarthquakeAsyncLoader(this, USGS_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Earthquake>> loader, List<Earthquake> earthquakes) {
+        // Find a reference to the {@link ListView} in the layout
+        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+        // Create a new {@link ArrayAdapter} of earthquakes
+        adapter = new EarthquakeListAdapter(EarthquakeActivity.this, earthquakes);
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        earthquakeListView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Earthquake>> loader) {
+        adapter.clear();
+    }
+
+    /**
+     * Executes a HTTP request in a background threat to load earthquake data using a loader.
      * Takes a URL as input parameter and outputs a list of earthquake objects
      */
-    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>> {
+    private static class EarthquakeAsyncLoader extends AsyncTaskLoader<List<Earthquake>> {
 
-        @Override
-        protected List<Earthquake> doInBackground(String... strings) {
-            ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes(strings[0]);
-            return earthquakes;
+        String url;
+
+        public EarthquakeAsyncLoader(Context context, String url) {
+            super(context);
+            this.url = url;
         }
 
         @Override
-        protected void onPostExecute(List<Earthquake> earthquakes) {
-            // Find a reference to the {@link ListView} in the layout
-            ListView earthquakeListView = (ListView) findViewById(R.id.list);
-
-            // Create a new {@link ArrayAdapter} of earthquakes
-            EarthquakeListAdapter adapter = new EarthquakeListAdapter(EarthquakeActivity.this, earthquakes);
-
-            // Set the adapter on the {@link ListView}
-            // so the list can be populated in the user interface
-            earthquakeListView.setAdapter(adapter);
+        public List<Earthquake> loadInBackground() {
+            ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes(url);
+            return earthquakes;
         }
     }
 }
